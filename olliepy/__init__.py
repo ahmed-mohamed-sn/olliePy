3f456eba437ff8dbf1c6fb3ba9198d85cf58aa31
Ahmed Mohamed <hanoush87@gmail.com>
2020-10-25 17:46:05 +0100

    Edited pypi readme and CI workflow will on run on master push

--- olliepy/__init__.py
+++ olliepy/__init__.py
@@ -1,5 +1,5 @@
 from .RegressionErrorAnalysisReport import RegressionErrorAnalysisReport
 from .InteractiveDashboard import InteractiveDashboard
 
-__version__ = '0.2'
+__version__ = '0.2.1'
 __all__ = ['__version__', 'RegressionErrorAnalysisReport', 'InteractiveDashboard']
