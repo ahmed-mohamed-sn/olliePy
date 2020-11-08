e4d3249e3f2430efff3156c0e1a761c8cceced22
Ahmed Mohamed <hanoush87@gmail.com>
2020-11-08 20:14:35 +0100

    Republishing because of github actions issue

--- sphinxSource/source/conf.py
+++ sphinxSource/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Ahmed Mohamed'
 author = 'Ahmed Mohamed'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
--- tests/test_olliepy.py
+++ tests/test_olliepy.py
@@ -2,7 +2,7 @@ from olliepy import __version__, __all__
 
 
 def test_version():
-    assert __version__ == '0.2.3'
+    assert __version__ == '0.2.4'
 
 
 def test_modules():
