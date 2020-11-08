21f29b5b1660b813c7f4189b26922c87d5f12685
Ahmed Mohamed <hanoush87@gmail.com>
2020-11-08 18:57:44 +0100

    Bypass reports cache and added clarity for usage

--- olliepy/__init__.py
+++ olliepy/__init__.py
@@ -1,5 +1,5 @@
 from .RegressionErrorAnalysisReport import RegressionErrorAnalysisReport
 from .InteractiveDashboard import InteractiveDashboard
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __all__ = ['__version__', 'RegressionErrorAnalysisReport', 'InteractiveDashboard']
